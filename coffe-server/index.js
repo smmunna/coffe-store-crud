008cc151f6d75165b19d3863760396e97d437be4
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-08 20:26:47 +0600

    updated

--- coffe-server/index.js
+++ coffe-server/index.js
@@ -3,14 +3,20 @@ const app = express()
 const port =  process.env.PORT || 5000
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
 
 // For Running Locally
-const uri = "mongodb://127.0.0.1:27017";
+// const uri = "mongodb://127.0.0.1:27017";
 
+// Running using online Database;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.rvrwrto.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
