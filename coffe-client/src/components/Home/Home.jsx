421bd6ead76bffa6d3635525a71baee6715a1018
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-08 14:36:34 +0600

    Update Home.jsx

--- coffe-client/src/components/Home/Home.jsx
+++ coffe-client/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import './Home.css'
 import Features from './Features/Features';
 import { Row } from 'react-bootstrap';
 import Products from './Products/Products';
+import CoofeImg from '../../assets/images/icons/1.png'
 
 const Home = () => {
     return (
@@ -18,6 +19,10 @@ const Home = () => {
 
             {/* Items list with Mapping; */}
            <div className='container mt-5'>
+            <div className='text-center my-4'>
+                <h3 className='display-5'>Our Popular Products </h3>
+                <button className='text-center'>Add Coffe <img src={CoofeImg} width={20} alt="" /></button>
+            </div>
            <Row xs={1} md={2} className="">
                 <Products/>
                 <Products/>
